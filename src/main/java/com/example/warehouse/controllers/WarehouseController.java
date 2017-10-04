6b43dc95a7def040303250053139ff820a44a3c4
exosyphon <exosyphon@yahoo.com>
2017-10-03 22:53:32 -0500

    Add view model for warehouse

--- src/main/java/com/example/warehouse/controllers/WarehouseController.java
+++ src/main/java/com/example/warehouse/controllers/WarehouseController.java
@@ -3,6 +3,7 @@ package com.example.warehouse.controllers;
 import com.example.warehouse.domain.Warehouse;
 import com.example.warehouse.repos.WarehouseRepository;
 import com.example.warehouse.services.WarehouseService;
+import com.example.warehouse.viewmodels.WarehouseResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -11,28 +12,35 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+import static java.util.Arrays.asList;
+
 @RestController
 @CrossOrigin
 public class WarehouseController {
 
-    private WarehouseRepository warehouseRepository;
-    private WarehouseService warehouseService;
+  private WarehouseRepository warehouseRepository;
+  private WarehouseService warehouseService;
 
-    @Autowired
-    public WarehouseController(WarehouseRepository warehouseRepository, WarehouseService warehouseService) {
-        this.warehouseRepository = warehouseRepository;
-        this.warehouseService = warehouseService;
-    }
+  @Autowired
+  public WarehouseController(WarehouseRepository warehouseRepository, WarehouseService warehouseService) {
+    this.warehouseRepository = warehouseRepository;
+    this.warehouseService = warehouseService;
+  }
 
-    @RequestMapping("/")
-    public ResponseEntity<String> index() {
-        warehouseRepository.save(new Warehouse(null, "wow it works"));
-        warehouseRepository.save(new Warehouse(null, " with things"));
+  @RequestMapping("/")
+  public ResponseEntity<List<WarehouseResponse>> index() {
+    warehouseRepository.save(new Warehouse(null, "wow it works"));
+    warehouseRepository.save(new Warehouse(null, " with things"));
 
-        List<Warehouse> themAll = warehouseService.getThemAll();
-        Warehouse warehouse1 = themAll.get(0);
-        Warehouse warehouse2 = themAll.get(1);
+    List<Warehouse> themAll = warehouseService.getThemAll();
+    Warehouse warehouse1 = themAll.get(0);
+    Warehouse warehouse2 = themAll.get(1);
 
-        return ResponseEntity.ok(warehouse1.getName() + warehouse2.getName());
-    }
+    return ResponseEntity.ok(
+        asList(
+            new WarehouseResponse(warehouse1.getId(), warehouse1.getName()),
+            new WarehouseResponse(warehouse2.getId(), warehouse2.getName())
+        )
+    );
+  }
 }
--- src/test/java/com/example/warehouse/controllers/WarehouseControllerTest.java
+++ src/test/java/com/example/warehouse/controllers/WarehouseControllerTest.java
@@ -3,6 +3,7 @@ package com.example.warehouse.controllers;
 import com.example.warehouse.domain.Warehouse;
 import com.example.warehouse.repos.WarehouseRepository;
 import com.example.warehouse.services.WarehouseService;
+import com.example.warehouse.viewmodels.WarehouseResponse;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,6 +12,8 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
+import java.util.List;
 
 import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
@@ -36,9 +39,14 @@ public class WarehouseControllerTest {
     public void testIndex() {
         when(mockWarehouseRepository.findAll()).thenReturn(asList(new Warehouse(1L, "name"), new Warehouse(2L, "other name")));
 
-        ResponseEntity<String> response = warehouseController.index();
+        ResponseEntity<List<WarehouseResponse>> response = warehouseController.index();
 
         assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(response.getBody(), asList(
+            new WarehouseResponse(1L, "name"),
+            new WarehouseResponse(2L, "other name")
+        ));
+
         verify(mockWarehouseRepository).findAll();
         verify(mockWarehouseService).getThemAll();
     }
