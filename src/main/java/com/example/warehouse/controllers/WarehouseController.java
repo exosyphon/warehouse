919a9b60a2e710de711a21ccdfa75ee69325cd7f
exosyphon <exosyphon@yahoo.com>
2017-10-03 20:52:12 -0500

    initial commit

---
+++ src/main/java/com/example/warehouse/controllers/WarehouseController.java
@@ -0,0 +1,15 @@
+package com.example.warehouse.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class WarehouseController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
