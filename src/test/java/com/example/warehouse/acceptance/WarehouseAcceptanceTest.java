aea83f92d534a04036bb5f872c2456286f99a5f1
exosyphon <exosyphon@yahoo.com>
2018-03-03 12:25:41 -0600

    Remove unneeded AutoConfiguration

--- src/test/java/com/example/warehouse/acceptance/WarehouseAcceptanceTest.java
+++ src/test/java/com/example/warehouse/acceptance/WarehouseAcceptanceTest.java
@@ -6,7 +6,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -18,7 +17,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@EnableAutoConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 public class WarehouseAcceptanceTest {
